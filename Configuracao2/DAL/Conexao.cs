864cd65e623ba617cde58c20601a2c22390939da
Dimas <123428856+DimasMota@users.noreply.github.com>
2023-02-09 22:00:07 -0300

    09_02_2023 03

---
+++ Configuracao2/DAL/Conexao.cs
@@ -0,0 +1,17 @@
+﻿
+
+using System.Reflection.Metadata;
+
+namespace DAL
+{
+    public static class Conexao
+    {
+        public static string StringDeConexao
+        {
+            get // funçao de apenas pegar dados e nao colocar
+            {
+                return @"User ID=SA, Initial Catalog=GESTAO, Data Source=.\SQLEXPRESS2019;Password=Senailab02";
+            }
+        }
+    }
+}
