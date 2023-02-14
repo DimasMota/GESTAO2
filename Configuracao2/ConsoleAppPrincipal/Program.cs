39de7fb79261e12181e3aad297a82bee438e608c
Dimas <123428856+DimasMota@users.noreply.github.com>
2023-02-13 22:03:55 -0300

    13-02-1-2023
    Mais um bocado de coisas

--- Configuracao2/ConsoleAppPrincipal/Program.cs
+++ Configuracao2/ConsoleAppPrincipal/Program.cs
@@ -1,11 +1,22 @@
-﻿namespace ConsoleAppPrincipal
+﻿using BLL;
+using Models;
+
+namespace ConsoleAppPrincipal
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            
-            
+            UsuarioBLL usuarioBLL = new UsuarioBLL();
+            Usuario usuario = new Usuario();
+            usuario.Nome = "Dimas Martins";
+            usuario.NomeUsuario = "dimasmota";
+            usuario.DataNascimento = "20-12-1986";
+            usuario.Cpf = "000.000.000-00";
+            usuario.Senha = "1Dimas@";
+            usuario.Email = "dimasmartinsmota@hotmail.com";
+            usuario.Ativo = true;
+            usuarioBLL.Inserir(usuario);
         }
     }
 }
