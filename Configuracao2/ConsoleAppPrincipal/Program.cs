f9107f0afb8d5b98410d0c2447804afa868c2959
Dimas <123428856+DimasMota@users.noreply.github.com>
2023-02-28 19:17:45 -0300

    28-02-2023 ref ao dia anterior

--- Configuracao2/ConsoleAppPrincipal/Program.cs
+++ Configuracao2/ConsoleAppPrincipal/Program.cs
@@ -11,56 +11,67 @@ namespace ConsoleAppPrincipal
             do
             {
                 Console.WriteLine("CADASTRO DE USUÁRIO\n\n");
-                Console.WriteLine("[1]USUÁRIO\n[2]GRUPO DE USUÁRIO\n[3]PERMISSÕES\n[0]SAIR");
+                Console.WriteLine("[1]USUÁRIO\n" +
+                                  "[2]GRUPO DE USUÁRIO\n" +
+                                  "[3]PERMISSÕES\n" +
+                                  "[0]SAIR");
                 Console.WriteLine("Olá Mundo");
                 opc_menu = Convert.ToInt32(Console.ReadLine());
 
 
                 if (opc_menu == 1)
                 {
-
-
-
-
-                    string opc_usuario;
+                    int opc_usuario;
                     Console.WriteLine("[1]CADASTRAR USUÁRIO\n" +
                                       "[2]ALTERAR USUÁRIO\n" +
                                       "[3]BUSCAR USUÁRIO\n" +
                                       "[4]EXCLUIR USUÁRIO\n" +
                                       "[0]SAIR\n");
 
-                    opc_usuario = Console.ReadLine().ToUpper();
-                    if (opc_usuario == "A")
+                    opc_usuario = Convert.ToInt32(Console.ReadLine());
+                    if (opc_usuario == 1)
                     {
 
                         try
                         {
                             Usuario usuario = new Usuario();
                             UsuarioBLL usuarioBLL = new UsuarioBLL();
-                            
+                            string sim_nao;
+                            do
+                            {
+
+                                Console.WriteLine("CADASTRO DE USUÁRIO\n\n");
+                                Console.WriteLine("Nome Completo: ");
+                                usuario.Nome = Console.ReadLine();
+                                Console.WriteLine("Nome de acesso do Usuário");
+                                usuario.NomeUsuario = Console.ReadLine();
+                                Console.WriteLine("Data de Nascimento");
+                                usuario.DataNascimento = Console.ReadLine();
+                                Console.WriteLine("CPF do Usuário: ");
+                                usuario.Cpf = Console.ReadLine();
+                                Console.WriteLine("Senha:");
+                                usuario.Senha = Console.ReadLine();
+                                Console.WriteLine("Digite seu E_mail");
+                                usuario.Email = Console.ReadLine();
+                                Console.WriteLine("Usuario Ativo [S]SIM  [N]NÃO :");
+                                usuario.Ativo = Console.ReadLine().ToUpper() == "S";
+
+
+                                Console.WriteLine("Deseja Salvar Usuário?");
+                                 sim_nao = Console.ReadLine().ToUpper();
+                                if (sim_nao == "S")
+                                {
+                                    usuarioBLL.Inserir(usuario);
+
+                                }
+
+                                Console.WriteLine("Deseja cadastrar outro Usuário?: [S]SIM [N]NÃO");
+                                sim_nao = Console.ReadLine().ToUpper();
+                            } while(sim_nao == "S");
+
+
+
                            
-                            Console.WriteLine("CADASTRO DE USUÁRIO\n\n");
-                            Console.WriteLine("Nome Completo: ");
-                            usuario.Nome = Console.ReadLine();
-                            Console.WriteLine("Nome de acesso do Usuário");
-                            usuario.NomeUsuario = Console.ReadLine();
-                            Console.WriteLine("Data de Nascimento");
-                            usuario.DataNascimento = Console.ReadLine();
-                            Console.WriteLine("CPF do Usuário: ");
-                            usuario.Cpf = Console.ReadLine();
-                            Console.WriteLine("Senha:");
-                            usuario.Senha = Console.ReadLine();
-                            Console.WriteLine("Digite seu E_mail");
-                            usuario.Email = Console.ReadLine();
-                            Console.WriteLine("Usuario Ativo [S]SIM  [N]NÃO :");
-                            usuario.Ativo = Console.ReadLine().ToUpper() == "S";
-
-
-
-
-                            usuarioBLL.Inserir(usuario);
-                            
-                            Console.WriteLine("CADASTRO DE USUÁRIO\n\n");
 
                         }
                         catch (Exception ex)
@@ -73,11 +84,11 @@ namespace ConsoleAppPrincipal
                 }
 
 
-                
 
 
 
-                      
+
+
 
             } while (opc_menu != 0);
 
